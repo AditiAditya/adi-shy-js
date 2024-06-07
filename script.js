485df62cecf5fe86ac75bfceab099012df310573
AditiAditya <163454278+AditiAditya@users.noreply.github.com>
2024-06-07 19:48:09 +0530

    commit 2

---
+++ script.js
@@ -0,0 +1,22 @@
+// var Ans = prompt('Name Please?');
+// Ans = prompt('Rename......');
+// alert(Ans);
+
+
+
+// var name1 = prompt('Sir your First Name ');
+// var name2 = prompt('Second Name  sir,');
+
+// console.log(name1);
+// console.log(name2);
+
+
+// var username = prompt('User name please ?');
+// var age = prompt('user age?');
+
+
+// var username = prompt('user id');
+// var age = prompt('user age');
+// var nowaliage = Number(age);
+
+// console.log( nowaliage + 3);
