adc25d8215bc8d9a64157500807271021b58025a
AditiAditya <163454278+AditiAditya@users.noreply.github.com>
2024-06-09 12:38:38 +0530

    array+pract.

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-// var username = Number(prompt ('give me your no.') );
-// console.log (username + 5);
+// ARRAY ==> defe. > A data stroage which can store one or more then one data togrther.
+// when you have more then one detale to store , and those detale are related to each other , at this point of time you can store in called array.
 
-// conditionals ==>' AGAR ' yesa ho to 'YASA'  karo ----  NAHI to 'yesa' karo
-// if    -   agar
-// else   -  nahi to
+// var arra = [" hari " , " harry " , " hemant " , " rohit "]
 
-var username = prompt('user name plerase ?');
+// console.log(arra[0]+" is "+ arra[2]+" best friend")
 
-// if( username == "Aditya"){
-//     console.log("!! HELLO BOSS !!")
-// }
+// terenary operater ==> Condition  ? do somthing when condition is true :  do somthing when condition is false .
+// eg. 
 
-
-// else{
-//     console.log("Hello Sir, How can i help you ");
-// }
+// (11<14) ? console.log("yes you are correct ") : console.log("no  you are wrong ") 
 
 
-//aapan kitna bhi eles if  kitna bhi bana sakte hai per usse phele if creat karna jaruri hai
+// var arra = ["hari" , " arry" , "hemant" , "rohit" , "rohan"]
 
-// if (username ==  'Radhe'){
-//     console.log("Maha rani shri raghika rani")
-// }
-// else if(username == "Krishna"){
-//     console.log("Shri jii ke dass")
-// }
+// console.log(arra[4])
 
-// else{
-//     console.log("Radhe Radhe")
-// }
+// var arra = ["hari" , " arry" , "hemant" , "rohit" , "rohan"]
+
+// console.log(arra[2] + " is my best friend")
+
+// var arra = ["hari" , " arry" , "hemant" , "rohit" , "rohan"]
+
+
+// console.log(arra[4] + "'s  best friend name is riya.")
+
+// var arra = ["hari" , " arry" , "hemant" , "rohit" , "rohan"]
+
+ //Que => hemant is common friend of arry and hari.
+
+//  console.log(arra[2] + " is a common friend of " + arra[1] + " and " + arra[0])
