bca8a84375fbe26dcadbfbff272fc4a91f0966e1
AditiAditya <163454278+AditiAditya@users.noreply.github.com>
2024-06-13 20:45:03 +0530

    test ques

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
-// // ##LOOP##
-// //Ans=>EK HI CHEEJ KO BAAR BAAR REPERT KARNA
+// // QUES => Make an array of 5 elements then remove the last element of array.
 
-// // var user = ['harsh', 'rohit', 'ram '];
-// // user.forEach(function (value) {
-// //     ;
-// //     console.log(value + ' is good ! ')
-// // })
+// var city = ["Indore" , "Surat" , "Navi Mumbai" , "Visakhapatnam" , "Bhopal"];
+// city.pop( )
+// console.log(city)
 
-//     foreach use case : whenever you have aaray and you want to perrform a particular operation on every member of the aaray.
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+//// QUES=> Make the array of 10 elements and remove the first element of array.
 
+// var city = ["Indore" , "Surat" , "Vijayawada" , "Pune" , "Navi Mumbai" , "Raipur" , "Bhopal" , "Vadodara" , "Visakhapatnam" , "Ahmedabad"]
+// city.shift()
+// console.log(city)
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+//// QUES=> Make the array of 9 elements and  add one another element at last of array.
+// var city = ["Indore" ,"Surat", "Vijayawada", "Pune", "Navi Mumbai", "Raipur", "Bhopal", "Vadodara", "Visakhapatnam"]
+// city.push( "Ahmedabad")
+// console.log(city)
 
-///////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//QUES=> Make the array of 4  elements and  add one another element at first of an  array.
+// var city = ["Surat", "Navi Mumbai", "Visakhapatnam", "Bhopal"];
+// city.unshift("Indore")
+// console.log(city)
