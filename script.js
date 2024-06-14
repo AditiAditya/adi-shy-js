e0fbda975d93c01b1ca0bd8139725a1ede2e4247
AditiAditya <163454278+AditiAditya@users.noreply.github.com>
2024-06-14 21:05:47 +0530

    Update script.js
    loop

--- script.js
+++ script.js
@@ -25,3 +25,21 @@
 // var city = ["Surat", "Navi Mumbai", "Visakhapatnam", "Bhopal"];
 // city.unshift("Indore")
 // console.log(city)
+
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+//QUES=> FIND THE LENGTH OF AN ARRAY.
+// var city = ["Indore" , "Surat" , "Vijayawada" , "Pune" , "Navi Mumbai" , "Raipur" , "Bhopal" , "Vadodara" , "Visakhapatnam" , "Ahmedabad"]
+// var len = [city.length]
+// console.log(len)
+
+//...................................................................................................................................................
+
+// for (let an = -1; an<12; an++){
+//     console.log(an)
+// }
+
+for (let x = 0; x<110; x++){
+
+    console.log(x+1)
+}
