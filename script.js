c794daa5f148f2889c450c1b57426b556eaae2b6
AditiAditya <163454278+AditiAditya@users.noreply.github.com>
2024-06-25 01:00:12 +0530

    ARRAY + METHOD

--- script.js
+++ script.js
@@ -1,220 +1,77 @@
-// // QUES => Make an array of 5 elements then remove the last element of array.
+//-----------------------------------------!!ARRAY!!----------------------------------------------------//
 
-// var city = ["Indore" , "Surat" , "Navi Mumbai" , "Visakhapatnam" , "Bhopal"];
-// city.pop( )
-// console.log(city)
+// BASIC OF ARRAY 
+// let class_12 =  ["Arjun" , "Payal" , "Surbhi" , "Mayank" , "Kunal"]
+// console.log(class_12[0]);
+// console.log(class_12[1]);
+// console.log(class_12[2]);
+// console.log(class_12[4]);
+// console.log(class_12[5]);
+// class_12[0] = "Ram" adding element in arrey
+// console.log(class_12);
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                                                                             //ARRAY METHODS 
 
-//// QUES=> Make the array of 10 elements and remove the first element of array.
+//let class_12 = ["Arjun", "Payal", "Surbhi", "Mayank", "Kunal", "Shuban", "Radhika", "Eklaviya", "Harry"]
 
-// var city = ["Indore" , "Surat" , "Vijayawada" , "Pune" , "Navi Mumbai" , "Raipur" , "Bhopal" , "Vadodara" , "Visakhapatnam" , "Ahmedabad"]
-// city.shift()
-// console.log(city)
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// console.log(class_12)
 
-//// QUES=> Make the array of 9 elements and  add one another element at last of array.
-// var city = ["Indore" ,"Surat", "Vijayawada", "Pune", "Navi Mumbai", "Raipur", "Bhopal", "Vadodara", "Visakhapatnam"]
-// city.push( "Ahmedabad")
-// console.log(city)
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-//QUES=> Make the array of 4  elements and  add one another element at first of an  array.
-// var city = ["Surat", "Navi Mumbai", "Visakhapatnam", "Bhopal"];
-// city.unshift("Indore")
-// console.log(city)
-
-// :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
-//QUES=> FIND THE LENGTH OF AN ARRAY.
-// var city = ["Indore" , "Surat" , "Vijayawada" , "Pune" , "Navi Mumbai" , "Raipur" , "Bhopal" , "Vadodara" , "Visakhapatnam" , "Ahmedabad"]
-// var len = [city.length]
-// console.log(len)
-
-//...................................................................................................................................................
-
-// for (let an = -1; an<12; an++){
-//     console.log(an)
-// }
-
-// for (let x = 0; x<110; x++){
-
-//     console.log(x+1)
-// }
-
-/////////////////////   aditi's code ///   very helpful
-// let a =[1,2,3,4,5,6,7,6,5,4,3,21,]
-
-// let a1 = a.filter((x) => x%2===0)
-// console.log(a1)
-
-// function arr(){
-//     let a = "aditi"
-//     console.log(arr(a))
-// }
-
-////////////////////////////////////////////////////////#Operators and Expressions////////////////////////////////////////////////////////////////////////
-
-//   let x =150;
-//   y = 5;
-// console.log("  x + y = ", x  +  y)
-// console.log("  x - y = ", x  -  y)
-// console.log("  x * y = ", x  *  y)
-//  console.log("  x / y = ", x  /  y)
-
-// console.log("  x ** y = ", x  **  y)// exponentia => [isme y, x ka squre ban jayga]
-
-// console.log("  x % y = ", x  %  y)// modules=> [isme divide ka remainder aata ha. (remainder ka pata chalta hai)]
-
-// value of a is 2 turn into 3
-// console.log("x++  =", x++)  //[value +1 ho jati hai ]
-
-/////////////////////////////////////////////////// ASSIGNMENT OPERATORS/////////////////////////////////////////
-
-//  let assi =11;
-
-// assi+=4             //+=
-// console.log(assi)
-
-// assi-=7             //-=
-// console.log( assi)
-
-// assi *=5
-// console.log( assi)
-
-// assi /=1
-// console.log( assi)
-
-///////////////////////////////////////////////////////#COMPARISON OPERATOR////////////////////////////////////////////////////////////////////////
-/*let ashi1 =10;
-ashi2= 9.4;
-
-console.log("ashi1 == ashi2 is " ,  ashi1 == ashi2 );
-console.log("ashi1 == ashi2 is " ,  ashi1 != ashi2 );
-console.log("ashi1 == ashi2 is " ,  ashi1 === ashi2 );
-console.log("ashi1 == ashi2 is " ,  ashi1 !== ashi2 );
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#TO STRING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// ----convert into string
+/* 
+let b = class_12.toString() 
+console.log(b);
 */
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#JOIN!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//------sare array element ko saperat kar deta hai eg. (or) run program.
+/*let c = class_12.join(" or ")
+console.log(c);
 
-/////////////////////////////////////////////////////////#LOOPS///////////////////////////////////////////////////////////////////////////////////////////
+!!!!!!!!!!!!!!!  POP( ) :- REMOVE ELEMENT FROM LAST OF ARRAY 
 
-// let sum = 0
-// let n = prompt("Enter the value of n ")
-// n = Number.parseInt(n) 
-// for (let x = 1; x <=n; x++) {
-//     sum += (x + 1)
-//     console.log(x)
-// }
-// console.log("Sum the first " + n + "natural number is " + sum)
+PUSH( ) ;- ADD ELEMENT IN LAST OF ARRAY 
 
-/*
-let obj ={
-    shiv: 99,
-    shivika: 88,
-    ram:89,
-    raj:78,
-    rohan: 67,
-}
-// for in loop
+SHIIFT( ) :- REMOVE ELEMENT IN FRONT OF  ARRAY 
 
-for  (let a in obj){
-    console.log( " Marks of " + a +  " are " +obj[a] )
-}
-
-//for of loop
-for (let b of "Aadi") {
-    console.log(b)
-}
-    */
-
-////////////////////////////////////////////////////////////# WHILE LOOPS///////////////////////////////////////////////////////////////////////////////////////////////
-/*
-let n = prompt("Enter your luckey number ")
-n = Number.parseInt(n)
-let i = 0;
-while(i<n){
-    console.log(i)
-    i++;
-}
+UNSHIFT( ) :- ADD ELEMENT IN FRONT  OF ARRAY  !!!!!!!!!!!!!!!!!!!!!!!!!!!!
 */
-////////////////////////////////////////////////////////////# DO-WHILE LOOPS///////////////////////////////////////////////////////////////////////////////////////////////
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#DELETE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//---------USE TO DELETE AN ELEMENT FROM ARRAY
 /*
-let n = prompt("Enter your luckey number ")
-n = Number.parseInt(n)
-
-let i = 10;
-do{
-    console.log(i)
-    i++;
-}while(i < n)*/
-
-//-----------------------------------------------------------------------------PRACTICE-----------------------------------------------------------------------------------------
-
-//IF ,ELES , ELSE IF.
-
-//QUES=> Make a "Good day" greeting if the hour is less than 18:00:
-
-// var Time = prompt("Correct Time Please")
-
-// if (Time == "18:00 ") {
-//     alert(Time)
-//     console.log("Good day")
-// }
-/////////////////////////////////////////////////////////////////#FUNCTIONS/////////////////////////////////////////////////////////////////////////////
-//WITHOUT FUNCTION HUM YESE INDIVISUAL CODE LIKHANGAE EK EK KA NAAM DE KE OR AGAR KUCH CHANGE KARNA HO TO EK EK KARKE KARNA PARAGA
-/*
-function Naam(name, secText) {
-    console.log(secText + " " + name);
-    console.log(n + " is a good boy");
-}
-
-let n = "Noman"
-let n2 = "Ansh"
-let n3 = "Adarsh"
-let n4 = "Amal"
-let secText = "Good Moring ";
-Naam(n, secText);
-Naam(n2, secText);
-Naam(n3, secText);
-Naam(n4, secText);
+delete class_12[5]  
+console.log(class_12);
+//console.log(class_12.length); 
 */
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#CONCATE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//Ading two or more then two array
+ //let class_12_marks = [48, 88, 89, 95, 66, 74, 69, 70, 66] ;
 
-//KABHI  ISKO  TRY MAT KARNA "DO NOT TRY AT ANY WHERE"
+// let class_12_3D= [458, 858, 589, 955, 665, 754, 569, 750, 656];
 
-/*console.log(n + " is a good boy");
-console.log(n2 + " is a bad boy");
-console.log(n3 + " is a not good  boy");
-console.log(n4 + " is a average boy");*/
-//...............................................................................................//............................................................................................//......................................
+// console.log(class_12_marks);
 
-// Template literals
+// let Result_12 = class_12.concat(class_12_marks)// adding array  
+// console.log(Result_12);
 
-////////////////////////////////////////////////make this with String
-//promode is a best friend of raghav
-// let boy1 = "promode"
-// let boy2  = "raghav"
-// console.log(boy1+" " +"is the best friend of raghav");
+// let  Parinam = class_12.concat(class_12_marks,class_12_3D)// Adding more then two element array
+// console.log(Parinam);
 
-///////////////////////////////////////////////// Escape Sequence Character
+//let class_12= [458  , 858, 589, 955, 665, 754  , 569, 750, 656];
 
-// let fruit = 'Pine\'apple'
-// console.log(fruit);
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#SHORT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//------IS IS USED TO ARRANGE THE ELEMENE IN ALPHABETICALLY
 
-// let fruit = "Pine\"apple"
-// console.log(fruit);
+//let class_12 = ["Arjun", "Payal", "Surbhi", "Mayank", "Kunal", "Shuban", "Radhika", "Eklaviya", "Harry"]
 
-//METHODS 
+// class_12.sort()
+// console.log(class_12);
 
-let  naam = " Harsh"
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#REVERCE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//--------Reverse the elements of array[ulta kar dena (a-z)=>(z-a)]
+/*l
+et class_12 = ["Arjun", "Payal", "Surbhi", "Mayank", "Kunal", "Shuban", "Radhika", "Eklaviya", "Harry"]
 
-/*naam.length // to find length
-console.log(naam)*/
-
-// naam.toUpperCase()  // to make all word capital 
-
-//aam.toLowerCase()   // to make all word capital 
-
-naam.slice(2,4)
-
-console.log(naam);
+class_12.reverse()
+console.log(class_12);
+*/
