48f1e3efdf9fe5e9b9c3c85bc951435e04d82643
AditiAditya <163454278+AditiAditya@users.noreply.github.com>
2024-06-28 23:40:50 +0530

    LOOPS

---
+++ loop.js
@@ -0,0 +1,36 @@
+//-----------------------------------------!!LOOPS!!----------------------------------------------------//
+// let sum = 0
+// let n = prompt("Enter the value of n")
+// n = Number.parseInt(n)
+// for (let x = 10; x < n; x++) {
+//     sum += (x + 1)
+//     console.log(x + 1)
+// }
+// console.log("Sum of first " +  n  + " natural number is " + sum);
+
+/////////////////////////////// FOR LOOP
+
+// for ( let n = 0; n <= 10; n++){
+//     console.log(n);
+//}
+
+//////////////////////////////// WHILE LOOPS
+
+// let n = prompt("Number")
+// n = Number.parseInt(n)
+
+// let i = 0 ; 
+// while(i<=n){
+//     console.log(i);
+//     i++
+//}
+//////////////////////////////// DO-WHILE LOOPS
+
+// let n = prompt("Number")
+// n = Number.parseInt(n)
+
+// let i = 0 ; 
+// do{
+//     console.log(i);
+//     i++
+// }while(i<=n)
