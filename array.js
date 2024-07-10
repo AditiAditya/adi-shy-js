a4d190abade0204434678848ade8898deba0963e
AditiAditya <163454278+AditiAditya@users.noreply.github.com>
2024-07-10 21:09:02 +0530

    arrays ques.1

---
+++ array.js
@@ -0,0 +1,78 @@
+// 1. Print All Elements of an Array*
+// Example: Given array [1, 2, 3, 4, 5], print all elements.
+// let arr= [1, 2, 3, 4, 5]
+// console.log(arr)
+
+// 2. Find the Length of an Array*
+//    - Example: Given array [1, 2, 3, 4, 5], find its length.
+// let arr= [1, 2, 3, 4, 5]
+// console.log(arr.length);
+
+// 3. *Access a Specific Element in an Array*
+//    - Example: Given array [1, 2, 3, 4, 5], access the third element.
+//    let arr = [1, 2, 3, 4, 5]
+//    console.log(arr[3]);
+
+//    4. *Add an Element to the End of an Array*
+//    - Example: Given array [1, 2, 3, 4, 5], add 6 to the end.
+// let arr= [1, 2, 3, 4, 5];
+// arr.push(6);
+// console.log(arr);
+
+// 5. *Remove the Last Element of an Array*
+//    - Example: Given array [1, 2, 3, 4, 5], remove the last element.
+// let arr= [1, 2, 3, 4, 5];
+// arr.pop();
+// console.log(arr);
+
+// 6. *Add an Element to the Beginning of an Array*
+//    - Example: Given array [1, 2, 3, 4, 5], add 0 to the beginning.
+// let arr= [1, 2, 3, 4, 5];
+// arr.unshift(0);
+// console.log(arr);
+
+// 7. *Remove the First Element of an Array*
+//    - Example: Given array [1, 2, 3, 4, 5], remove the first element.
+// let arr= [1, 2, 3, 4, 5];
+// arr.shift();
+// console.log(arr);
+
+// // 8. *Find the Index of an Element in an Array*
+// //    - Example: Given array [1, 2, 3, 4, 5], find the index of element 3
+// let arr = [1, 2, 3, 4, 5];
+// let ays = arr.indexOf(3)
+// console.log(ays);
+
+//9. *Check if an Array Contains a Specific Element*
+// - Example: Given array [1, 2, 3, 4, 5], check if it contains 3.
+// let arr= [1, 2, 3, 4, 5];0000000000000000000000000000000000000000000000000000000000000000
+
+// 11. Join Two Arrays*
+//     - Example: Given arrays [1, 2, 3] and [4, 5, 6], join them.
+// let arr= [1, 2, 3,]
+// let ays= [4, 5, 6]
+// let newNum=arr.concat(ays);
+// console.log(newNum);
+
+//10. *Convert an Array to a String*
+//  - Example: Given array [1, 2, 3, 4, 5], convert it to a string.
+// let arr= ["1", "2", "3", "4", "5"]
+// let a = arr.toString();
+// console.log(a)
+
+// 12. *Create an Array from a String*
+// Example: Given string "hello", create an array of characters.
+// let a = ["Hello"]
+// let b = a.split();
+// console.log(b); 0000000000000000000000000000000000000000000000000000000000000000000
+
+
+
+
+
+
+
+let fruit = ["Apple" , "Mango" , "Banana" , "grapes" , "kiwi"]
+let copyfruit = fruit.slice
+
+console.log(fruit);
